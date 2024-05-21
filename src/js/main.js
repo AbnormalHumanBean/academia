070c145d7752b45a0b55d0f6dceffe4d17ac3ef8
Ivanna Porter <143569469+AbnormalHumanBean@users.noreply.github.com>
2024-05-20 19:17:52 -0500

    adding

--- src/js/main.js
+++ src/js/main.js
@@ -34,3 +34,64 @@ document.addEventListener('DOMContentLoaded', function() {
   let skillsSelector = '#skills-list .skill';
   updateProgressBars(skillsSelector);
 });
+
+import fs from 'fs';
+import PDFDocument from 'pdfkit';
+import ace from 'brace';
+import 'brace/mode/javascript';
+import 'brace/theme/monokai';
+import { waitForData } from './pdfkitHelpers.js';
+import { fetchFile } from './httpHelpers.js';
+// testImage is an URL
+// bundle font and image files and register them in the virtual fs
+import './registerStaticFiles.js';
+
+v
+fetchFile(testImageURL)
+  .then(testImageData => {
+    fs.writeFileSync('images/test.jpg', testImageData);
+  })
+  .catch(error => {
+    console.error(error);
+  });
+
+var initialFnCode = `// create a document
+var doc = new PDFDocument();
+
+// waitForData must be called before call to doc.end()
+waitForData(doc)
+  .then(dataUrl => {
+    // display the document in the iframe to the right
+    iframe.src = dataUrl;
+  })
+  .catch(error => {
+    console.log(error);
+  });
+
+doc.end();`;
+
+function executeFn(code, PDFDocument, lorem, waitForData, iframe) {
+  var fn = new Function('PDFDocument', 'lorem', 'waitForData', 'iframe', code);
+  fn(PDFDocument, lorem, waitForData, iframe);
+}
+
+var editor = ace.edit('editor');
+editor.setTheme('ace/theme/monokai');
+editor.getSession().setMode('ace/mode/javascript');
+editor.setValue(initialFnCode);
+editor
+  .getSession()
+  .getSelection()
+  .clearSelection();
+
+var iframe = document.querySelector('iframe');
+
+executeFn(initialFnCode, PDFDocument, lorem, waitForData, iframe);
+
+editor.getSession().on('change', function() {
+  try {
+    executeFn(editor.getValue(), PDFDocument, lorem, waitForData, iframe);
+  } catch (e) {
+    console.error(e);
+  }
+});
