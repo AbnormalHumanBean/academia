ff73ce81f949153f47d3dd0a2fded283457fbaa7
Ivanna Porter <143569469+AbnormalHumanBean@users.noreply.github.com>
2024-05-25 21:28:05 -0500

    icon redo

--- webpack.config.js
+++ webpack.config.js
@@ -83,7 +83,7 @@ module.exports = {
                         const nav = [
 									'<nav class="navbar fixed-top navbar-expand bg-body-tertiary bg-nav">',
 									'  <a class="navbar-brand" href="#">',
-									'    <img src="images/icon.png" alt="texas tech double t" width="35" height="35" />',
+									'    <img src="images/ip-icon.png" alt="the letter i with the letter p" width="35" height="35" />',
 									"  </a>",
 									'  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">',
 									'    <span class="navbar-toggler-icon"></span>',
