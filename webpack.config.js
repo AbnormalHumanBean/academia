47482f6fdf2d0a37855e03c92ba1f5f9d734f77d
Ivanna Porter <143569469+AbnormalHumanBean@users.noreply.github.com>
2024-06-03 18:41:58 -0500

    let's try

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 'use strict';
+const stylesheets = ['<link rel="stylesheet" href="css/styles.css">','<link rel="stylesheet" href="css/additions.css">','<link defer rel="stylesheet" href="css/icofont.css">','<link defer rel="stylesheet" href="css/icons.css">','<link defer rel="stylesheet" href="css/bootstrap-icons.css">','<script defer src="https://kit.fontawesome.com/33332c4d45.js" crossorigin="anonymous"></script>','<link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">','<link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">','<link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">','<link rel="manifest" href="favicon/site.webmanifest"></link>'];
+const nav = ['<nav class="navbar fixed-top navbar-expand-sm bg-body-tertiary bg-nav"> <a class="navbar-brand" href="#"> <img src="images/icon2.png" alt="the letter i with the letter p" width="35" height="35" /> </a> <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button> <div class="collapse navbar-collapse" id="navbarSupportedContent"> <ul class="navbar-nav me-auto mb-2 mb-lg-0"> <li class="nav-item"> <a class="nav-link data-bs-display="static" data-bs-toggle="tooltip" title="The landing Page" href="index.html">Welcome</a> </li> <li class="nav-item"> <a class="nav-link" href="about.html">About</a> </li> <li class="nav-item"> <a class="nav-link" href="research.html">Research</a> </li> <li class="nav-item dropdown"> <div class="btn-group"> <a class="split-nav-a dropdown-link nav-link" href="teaching.html">Teaching</a> <a class="split-nav-b dropdown-toggle dropdown-toggle-split nav-link" href="#" id="teach" role="button" data-bs-toggle="dropdown" aria-expanded="false"> </a> <ul class="dropdown-menu"> <li><a class="dropdown-item" href="teaching_tools.html">Tools & Examples</a></li> </ul> </div> </li> <li class="nav-item dropdown"> <a class="nav-link dropdown-toggle dropdown-link px-0 px-lg-2" href="#" id="code" role="button" data-bs-toggle="dropdown" aria-expanded="false"> CodeBox </a> <ul class="dropdown-menu"> <li><a class="dropdown-item" href="coding_show.html">Show Coding</a></li> <li><a class="dropdown-item" href="coding_explain.html">Explain Coding</a></li> </ul> </li> </ul> <ul class="navbar-nav ms-0 me-1"> <li class="nav-item"> <a class="nav-link" href="connect.html">Connect</a> <li class="nav-item"> <a class="nav-link" href="cv.html">CV</a> </li> <li class="nav-item py-2 px-1"> <div class="vr h-100 d-flex opacity-75"> </div> </li> <li class="nav-item dropdown"> <a href="#" class="nav-link px-0 px-lg-2 dropdown-toggle d-flex align-items-center show" id="bd-color-mode" data-bs-toggle="dropdown" aria-expanded="false" data-bs-display="static" data-bs-toggle="tooltip" title="Toggle color mode"> <span class="d-light"> <i class="bi bi-brightness-high-fill"></i> </span> <span class="d-dark"> <i class="bi bi-moon-stars-fill"></i> </span> <span class="d-auto"> <i class="bi bi-circle-half"></i> </span> </a> <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="bd-color-mode" style="--bs-dropdown-min-width: 6rem;" data-bs-popper="static"> <li> <button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="light" aria-pressed="false"> <i class="bi bi-brightness-high-fill"></i> <span class="ms-2">Light</span> </button> </li> <li> <button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="dark" aria-pressed="false"> <i class="bi bi-moon-stars-fill"></i> <span class="ms-2">Dark</span> </button> </li> <li> <button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="auto" aria-pressed="true"> <i class="bi bi-circle-half"></i> <span class="ms-2">Auto</span> </button> </li> </ul> </li> </ul> </div> </nav>'];
+
 const path = require('path');
 const webpack = require("webpack");
 const autoprefixer = require('autoprefixer');
@@ -20,7 +23,8 @@ module.exports = {
         main: './src/js/main.js',
         githubget: './src/js/githubget.js',
         html_highlight: './src/js/html_highlight.js',
-        adjust: './src/js/adjustment.js'
+        adjust: './src/js/adjustment.js',
+        adobe_api: './src/js/adobe_api.js'
     },
     output: {
         filename: '[name].js',
@@ -42,7 +46,8 @@ module.exports = {
         new HtmlWebpackPlugin(htmlWebpackPluginConfig('./src/coding_show.html', 'coding_show.html', ['main', 'githubget'])),
         new HtmlWebpackPlugin(htmlWebpackPluginConfig('./src/coding_explain.html', 'coding_explain.html', ['main'])),
         new HtmlWebpackPlugin(htmlWebpackPluginConfig('./src/connect.html', 'connect.html', ['main'])),
-        new HtmlWebpackPlugin(htmlWebpackPluginConfig('./src/cv.html', 'cv.html', ['main'])),
+        new HtmlWebpackPlugin(htmlWebpackPluginConfig('./src/cv.html', 'cv.html', ['main','adobe_api'])),
+        
         
         new CopyWebpackPlugin({
             patterns: [{
@@ -68,102 +73,10 @@ module.exports = {
             ],
         }),
         function() {
-            this.hooks.compilation.tap('HtmlWebpackPlugin', (compilation) => {
-                HtmlWebpackPlugin.getHooks(compilation).beforeEmit.tapAsync(
+            this.hooks.compilation.tap('HtmlWebpackPlugin', (inject) => {
+                HtmlWebpackPlugin.getHooks(inject).beforeEmit.tapAsync(
                     'InjectCssPlugin',
-                    (data, cb) => {
-                        const stylesheets = [
-                            '<link rel="stylesheet" href="css/styles.css">',
-                            '<link rel="stylesheet" href="css/additions.css">',
-                            '<link defer rel="stylesheet" href="css/icofont.css">',
-                            '<link defer rel="stylesheet" href="css/icons.css">',
-                            '<link defer rel="stylesheet" href="css/bootstrap-icons.css">',
-                            '<script defer src="https://kit.fontawesome.com/33332c4d45.js" crossorigin="anonymous"></script>',
-                            '<link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">',
-                            '<link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">',
-                            '<link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">',
-                            '<link rel="manifest" href="favicon/site.webmanifest"></link>',
-                        ];
-                        const nav = [
-									'<nav class="navbar fixed-top navbar-expand-sm bg-body-tertiary bg-nav">',
-									'  <a class="navbar-brand" href="#">',
-									'    <img src="images/icon2.png" alt="the letter i with the letter p" width="35" height="35" />',
-									"  </a>",
-									'  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">',
-									'    <span class="navbar-toggler-icon"></span>',
-									"  </button>",
-									'  <div class="collapse navbar-collapse" id="navbarSupportedContent">',
-									'    <ul class="navbar-nav me-auto mb-2 mb-lg-0">',
-									'      <li class="nav-item">',
-									'        <a class="nav-link data-bs-display="static" data-bs-toggle="tooltip" title="The landing Page" href="index.html">Welcome</a>',
-									"      </li>",
-									'      <li class="nav-item">',
-									'        <a class="nav-link" href="about.html">About</a>',
-									"      </li>",
-									'      <li class="nav-item">',
-									'        <a class="nav-link" href="research.html">Research</a>',
-									"      </li>",
-									'      <li class="nav-item dropdown">',
-									'        <div class="btn-group">',
-									'          <a class="split-nav-a dropdown-link nav-link" href="teaching.html">Teaching</a>',
-									'          <a class="split-nav-b dropdown-toggle dropdown-toggle-split nav-link" href="#" id="teach" role="button" data-bs-toggle="dropdown" aria-expanded="false"> </a>',
-									'          <ul class="dropdown-menu">',
-									'            <li><a class="dropdown-item" href="teaching_tools.html">Tools & Examples</a></li>',
-									"          </ul>",
-									"        </div>",
-									"      </li>",
-									'      <li class="nav-item dropdown">',
-									'        <a class="nav-link dropdown-toggle dropdown-link px-0 px-lg-2" href="#" id="code" role="button" data-bs-toggle="dropdown" aria-expanded="false">',
-									"          CodeBox",
-									"        </a>",
-									'        <ul class="dropdown-menu">',
-									'          <li><a class="dropdown-item" href="coding_show.html">Show Coding</a></li>',
-									'          <li><a class="dropdown-item" href="coding_explain.html">Explain Coding</a></li>',
-									"        </ul>",
-									"      </li>",
-									"    </ul>",
-									'    <ul class="navbar-nav ms-0 me-1">',
-                                    '<li class=nav-item>  <a class="nav-link" href="connect.html">Connect</a>',
-                                    '<li class=nav-item>  <a class="nav-link" href="cv.html">CV</a>',
-									"      </li>",
-                                    ' <li class="nav-item py-2 px-1"> <div class="vr h-100 d-flex opacity-75"> </div> </li>',
-									'      <li class="nav-item dropdown">',
-									'        <a href="#" class="nav-link px-0 px-lg-2 dropdown-toggle d-flex align-items-center show" id="bd-color-mode" data-bs-toggle="dropdown" aria-expanded="false" data-bs-display="static" data-bs-toggle="tooltip" title="Toggle color mode">',
-									'          <span class="d-light">',
-									'            <i class="bi bi-brightness-high-fill"></i>',
-									"          </span>",
-									'          <span class="d-dark">',
-									'            <i class="bi bi-moon-stars-fill"></i>',
-									"          </span>",
-									'          <span class="d-auto">',
-									'            <i class="bi bi-circle-half"></i>',
-									"          </span>",
-									"        </a>",
-									'        <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="bd-color-mode" style="--bs-dropdown-min-width: 6rem;" data-bs-popper="static">',
-									"          <li>",
-									'            <button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="light" aria-pressed="false">',
-									'              <i class="bi bi-brightness-high-fill"></i>',
-									'              <span class="ms-2">Light</span>',
-									"            </button>",
-									"          </li>",
-									"          <li>",
-									'            <button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="dark" aria-pressed="false">',
-									'              <i class="bi bi-moon-stars-fill"></i>',
-									'              <span class="ms-2">Dark</span>',
-									"            </button>",
-									"          </li>",
-									"          <li>",
-									'            <button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="auto" aria-pressed="true">',
-									'              <i class="bi bi-circle-half"></i>',
-									'              <span class="ms-2">Auto</span>',
-									"            </button>",
-									"          </li>",
-									"        </ul>",
-									"      </li>",
-									"    </ul>",
-									"  </div>",
-									"</nav>",
-								];
+                    (data, cb) => {stylesheets + nav;
                         data.html = data.html.replace(
                             "</head>",
                             stylesheets.join("\n") + "\n</head>" + "\n<body>" + nav.join("\n")
@@ -179,7 +92,8 @@ module.exports = {
           })
     ],
     module: {
-        rules: [{
+        rules: [
+            {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader'],
             },
@@ -203,10 +117,7 @@ module.exports = {
                 test: /\.svg$/,
                 loader: 'svg-url-loader',
                 options: {
-                    // Images larger than 10 KB won’t be inlined
                     limit: 10 * 1024,
-                    // Remove quotes around the encoded URL –
-                    // they’re rarely useful
                     noquotes: true,
                 }
             },
@@ -214,14 +125,17 @@ module.exports = {
                 test: /\.(jpe?g|png|gif)$/,
                 loader: 'url-loader',
                 options: {
-                    // Images larger than 10 KB won’t be inlined
-                    limit: 10 * 1024
+                        limit: 10 * 1024
                 }
             }
         ],
     },
     optimization: {
         minimize: true,
-        minimizer: [new TerserPlugin()],
+        minimizer: [new TerserPlugin({
+            terserOptions: {
+              keep_classnames: true,
+            },
+          }),],
       },
 };
