da926f1a7339adc3bdcb32305837cceaae17c5bc
Ivanna Porter <143569469+AbnormalHumanBean@users.noreply.github.com>
2024-07-27 20:23:38 -0500

    part 2

--- src/js/github_profile.js
+++ src/js/github_profile.js
@@ -1,29 +1,24 @@
-function loadGithubCard() {
-	const githubUsername = "AbnormalHumanBean";
-	const githubToken = process.env.GITHUB_TOKEN;
-	// Fetch user data from GitHub API
-	fetch(`https://api.github.com/users/${githubUsername}`, {
-			headers: {
-				'Authorization': `token ${githubToken}`
-			}
-		}).then(response => response.json())
-		.then(data => {
-			const {
-				login,
-				name,
-				avatar_url,
-			} = data;
-			// Update card content
-			document.querySelector('.ava').href = `https://github.com/${login}`;
-			document.querySelector('.ava img').src = `${avatar_url}&s=60`;
-			document.querySelector('.ava img').alt = name;
-			document.querySelector('#name-head-1').textContent = `${name}`;
-			document.querySelector('#name-head-2').textContent = `(${githubUsername})`;
-			document.querySelector('#linky').href =
-				`https://github.com/${login}`;
-			document.querySelector('#linky2').href =
-				`https://github.com/${login}`;
-		})
-		.catch(error => console.error('Error fetching user data:', error));
-};
+async function loadGithubCard() {
+	try {
+		 const response = await fetch('/.netlify/functions/fetchGithubUser');
+		 if (!response.ok) {
+			  throw new Error(`HTTP error! status: ${response.status}`);
+		 }
+
+		 const data = await response.json();
+		 const { login, name, avatar_url } = data;
+
+		 // Update card content
+		 document.querySelector('.ava').href = `https://github.com/${login}`;
+		 document.querySelector('.ava img').src = `${avatar_url}&s=60`;
+		 document.querySelector('.ava img').alt = name;
+		 document.querySelector('#name-head-1').textContent = `${name}`;
+		 document.querySelector('#name-head-2').textContent = `(${login})`;
+		 document.querySelector('#linky').href = `https://github.com/${login}`;
+		 document.querySelector('#linky2').href = `https://github.com/${login}`;
+	} catch (error) {
+		 console.error('Error fetching user data:', error);
+	}
+}
+
 document.addEventListener('DOMContentLoaded', loadGithubCard);
